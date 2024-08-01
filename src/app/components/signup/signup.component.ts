26583dcc110ace220f42c52bddf829f861595cf2
digitalartist221 <sall.aboubacre@gmail.com>
2024-08-02 01:46:12 +0200

    login register design

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators  } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../auth.service';
 
@@ -12,27 +12,41 @@ import { AuthService } from '../../auth.service';
 export class SignupComponent implements OnInit {
 //formulaire
 
-form !: FormGroup;
+registerForm !: FormGroup;
 constructor(private formBuilder : FormBuilder, private authService : AuthService, private router : Router){
 
 }
   ngOnInit(): void {
-    this.form = this.formBuilder.group({
-      name : '',
-      email: '',
-      password : ''
-    });
+    this.registerForm = this.formBuilder.group({
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      sexe: ['', Validators.required],
+      dateNaissance: ['', Validators.required],
+      region: ['', Validators.required],
+      departement: ['', Validators.required],
+      commune: ['', Validators.required],
+      adresee: ['', Validators.required],
+      telephone: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      role: ['user', Validators.required],  // Default role to 'user'
+       });
   }
 
   //soumission de (submit)
 
-  submit(){
+  onRegister(){
 
-    this.authService.register('http://127.0.0.1:8000/api/register').subscribe(res => {
-      //console.log(res);
-      this.router.navigate(['/login']); // on redirige
-    });
-
+    console.log("ici");
+      this.authService.register(this.registerForm.value).subscribe(
+        response => {
+          console.log('Registration successful', response);
+          this.router.navigate(['/login']);
+        },
+        error => {
+          console.error('Registration error', error);
+        }
+      );
   }
 
 }
